4781146042f1524ffa02c1ea236559631851ee7c
ds-an <ds-an@tuta.io>
2023-01-15 15:22:56 +0200

    Superfinal (for now, hopefully) commit. Hurray!

--- src/objects/items/OtherDrink.java
+++ src/objects/items/OtherDrink.java
@@ -15,9 +15,8 @@ public class OtherDrink extends Item{
 
     @Override
     public void insertItem() throws SQLException {
-        String query = String.format("INSERT INTO drinksother VALUES (NULL, '%s', '%s', %f, %f, %d, '%s')",
-                super.getItemName(), super.getItemType(),
-                super.getPriceInside(), super.getPriceOutside(), super.getTotalLeft(), super.getDescription());
+        String query = String.format("INSERT INTO drinksother VALUES (NULL, '%s', %f, %f, %d, '%s')",
+                super.getItemName(), super.getPriceInside(), super.getPriceOutside(), super.getTotalLeft(), super.getDescription());
         Database.inputData(query);
     }
 }
